e4fa8b161a21e22fa91be050145f51d9009b3b74
IlTetta <116306022+IlTetta@users.noreply.github.com>
2024-08-11 18:38:14 +0200

    Home doc

--- src/main/java/client/GUI/panels/Home.java
+++ src/main/java/client/GUI/panels/Home.java
@@ -12,21 +12,22 @@ import shared.utils.Interfaces;
  * La classe {@code Home} rappresenta il pannello principale dell'applicazione
  * visualizzato dopo il caricamento iniziale.
  * <p>
- * Questo pannello fornisce all'utente due opzioni principali: "Cerca e
- * visualizza dati" e "Gestisci area operatore".
+ * Questo pannello fornisce all'utente due opzioni principali:
+ * <ul>
+ *     <li>"Cerca e visualizza dati" per accedere alla funzionalità di ricerca e visualizzazione dei dati.</li>
+ *     <li>"Gestisci area operatore" per accedere alla gestione dell'area riservata agli operatori.</li>
+ * </ul>
  * </p>
  * <p>
- * L'utente pu&ograve; selezionare una delle opzioni per avviare
- * funzionalit&agrave;
- * specifiche dell'applicazione.
+ * L'utente può selezionare una delle opzioni per avviare le funzionalità specifiche dell'applicazione.
  * </p>
- * 
+ *
  * @see GUI
  * @see Widget
  * @see TwoRows
  * @see MainModel
  * @see Interfaces
- * 
+ *
  * @author Andrea Tettamanti
  * @author Luca Mascetti
  * @version 1.0
@@ -38,7 +39,7 @@ public class Home extends TwoRows implements Interfaces.UIPanel {
      * L'ID univoco di questo pannello. Viene utilizzato per identificare e navigare
      * tra i diversi pannelli dell'applicazione.
      */
-    public static String ID = "Home";
+    public static final String ID = "Home";
 
     /**
      * Riferimento all'interfaccia utente grafica (GUI) associata alla barra del
@@ -47,28 +48,33 @@ public class Home extends TwoRows implements Interfaces.UIPanel {
     private GUI gui;
 
     /**
-     * Pulsante per navigare alla funzionalit&agrave; di ricerca e visualizzazione
-     * dei
-     * dati.
+     * Pulsante per navigare alla funzionalità di ricerca e visualizzazione
+     * dei dati.
      */
     private final JButton buttonToFind = new Widget.Button("Cerca e visualizza dati");
 
     /**
-     * Pulsante per navigare alla funzionalit&agrave; di gestione dell'area
-     * riservata agli
-     * operatori.
+     * Pulsante per navigare alla funzionalità di gestione dell'area
+     * riservata agli operatori.
      */
     private final JButton buttonToOperator = new Widget.Button("Gestisci area operatore");
 
     /**
      * Crea una nuova istanza di {@code Home}.
-     *
+     * <p>
+     * Questo costruttore inizializza il pannello principale dell'applicazione. Non
+     * esegue operazioni aggiuntive al momento della creazione.
+     * </p>
      */
     public Home() {
     }
 
     /**
      * Aggiunge gli eventi di azione ai pulsanti dell'interfaccia utente.
+     * <p>
+     * Questo metodo assegna i listener ai pulsanti per gestire la navigazione tra
+     * i pannelli dell'applicazione quando vengono premuti.
+     * </p>
      */
     private void addActionEvent() {
         buttonToFind.addActionListener(e -> {
@@ -101,7 +107,17 @@ public class Home extends TwoRows implements Interfaces.UIPanel {
         return ID;
     }
 
+    /**
+     * Invocato quando il pannello viene aperto.
+     * <p>
+     * Questo metodo non esegue azioni specifiche al momento dell'apertura del
+     * pannello.
+     * </p>
+     *
+     * @param args Argomenti aggiuntivi (non utilizzati in questo caso).
+     */
     @Override
     public void onOpen(Object[] args) {
+        // Nessuna azione specifica al momento dell'apertura
     }
 }
