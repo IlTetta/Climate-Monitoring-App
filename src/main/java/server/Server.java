5dc88b5ff6dac1ac0c53fd2d5fedea86a0110fd5
IlTetta <116306022+IlTetta@users.noreply.github.com>
2024-08-14 13:39:09 +0200

    Server doc

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -10,19 +10,67 @@ import java.rmi.AlreadyBoundException;
 import java.net.MalformedURLException;
 
 /**
- * Classe principale per l'avvio del server RMI.
+ * La classe {@code Server} rappresenta il server principale che avvia e registra
+ * le implementazioni RMI per i vari servizi offerti dall'applicazione.
+ * <p>
+ *     Questo server avvia il registro RMI e pubblica le implementazioni delle
+ *     interfacce remote in modo che possano essere invocate dai client remoti.
+ * </p>
+ *
+ * <p>
+ *     Le interfacce implementate e registrate includono:
+ *     <ul>
+ *         <li>{@link DataQueryInterface} per l'interrogazione dei dati.</li>
+ *         <li>{@link DataHandlerInterface} per la gestione dei dati.</li>
+ *         <li>{@link LogicOperatorInterface} per la logica di business degli operatori.</li>
+ *         <li>{@link LogicCenterInterface} per la logica di business dei centri.</li>
+ *         <li>{@link LogicCityInterface} per la logica di business delle città.</li>
+ *     </ul>
+ * </p>
+ *
+ * <p>
+ *     Una volta avviato con successo, il server stampa un messaggio di conferma
+ *     sulla console.
+ * </p>
+ *
+ * @see DataQueryInterface
+ * @see DataHandlerInterface
+ * @see LogicOperatorInterface
+ * @see LogicCenterInterface
+ * @see LogicCityInterface
+ * @see DataQueryImp
+ * @see DataHandlerImp
+ * @see LogicOperatorImp
+ * @see LogicCenterImp
+ * @see LogicCityImp
+ *
+ * @author Andrea Tettamanti
+ * @author Luca Mascetti
+ * @author Manuel Morlin
+ * @version 1.0
+ * @since 14/08/2024
+ *
  */
 public class Server {
+
+    /**
+     * Il metodo principale per avviare il server.
+     * <p>
+     *     Questo metodo crea le istanze delle implementazioni RMI e le registra nel
+     *     registro RMI. In caso di successo, il server sarà in ascolto sulle
+     *     richieste dei client remoti. In caso di errore, vengono gestite le
+     *     eccezioni e viene stampato un messaggio di errore sulla console.
+     * </p>
+     * @param args Argomenti della riga di comando (non utilizzati).
+     */
     public static void main(String[] args) {
         try {
-            // Inizializzazione delle interfacce e delle loro implementazioni
             DataQueryInterface dataQuery = new DataQueryImp();
             DataHandlerInterface dataHandler = new DataHandlerImp(dataQuery);
             LogicOperatorInterface logicOperator = new LogicOperatorImp(dataHandler, dataQuery);
             LogicCenterInterface logicCenter = new LogicCenterImp(dataHandler, dataQuery);
             LogicCityInterface logicCity = new LogicCityImp();
 
-            // Creazione del registro RMI sulla porta 1099
             Registry registry = LocateRegistry.createRegistry(1099);
 
             // Registrazione delle implementazioni nel registro RMI
